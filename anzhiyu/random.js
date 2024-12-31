7eb7c1ae3e709ddfcc0854f4a3622b1b4a6476a3
superhe2004 <superhe2004@sina.com>
2025-01-01 01:03:00 +0800

    Site updated: 2025-01-01 01:02:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/29/xinceshi/","2024/12/29/hello-world/"];function toRandomPost(){
+var posts=["2025/01/01/laping1/","2025/01/01/posts/","2024/12/29/xinceshi/","2024/12/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
